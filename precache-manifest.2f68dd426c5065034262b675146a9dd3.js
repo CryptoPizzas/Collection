78cb68da66168e0d76831829481ccb05dd7c11f2
Gigi <aloysiusbozzoli@gmail.com>
2022-10-01 11:04:44 +0200

    Updates

---
+++ precache-manifest.2f68dd426c5065034262b675146a9dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aa89e46571772a41ea9",
+    "url": "/Collection/static/css/main.5dc201eb.chunk.css"
+  },
+  {
+    "revision": "6aa89e46571772a41ea9",
+    "url": "/Collection/static/js/main.6aa89e46.chunk.js"
+  },
+  {
+    "revision": "dca74a035a2640b5084a",
+    "url": "/Collection/static/js/1.dca74a03.chunk.js"
+  },
+  {
+    "revision": "58831caada2630b61808",
+    "url": "/Collection/static/js/2.58831caa.chunk.js"
+  },
+  {
+    "revision": "c1c1cf2644ab5ab03b80",
+    "url": "/Collection/static/js/runtime~main.c1c1cf26.js"
+  },
+  {
+    "revision": "f80b69469cae9e0ed7309301811d9865",
+    "url": "/Collection/index.html"
+  }
+];
